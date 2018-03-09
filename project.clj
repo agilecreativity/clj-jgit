130cb38136297757632e50f13362b3e6c3502ec9
Burin Choomnuan <agilecreativity@gmail.com>
2018-03-08 23:41:23 -0500

    Update version dependencies
    - As last publish version is at 0.8.10
    - I want to use Clojure 1.9.0 instead of 1.8.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-jgit "0.9.1-SNAPSHOT"
+(defproject clj-jgit "0.9.1"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "4.8.0.201706111038-r" :exclusions [com.jcraft/jsch]]
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "4.10.0.201712302008-r" :exclusions [com.jcraft/jsch]]
                  [com.jcraft/jsch "0.1.54"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}}
-  :plugins [[lein-marginalia "0.9.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :plugins [[lein-marginalia "0.9.1"]]
   :repositories {"jgit-repository" "https://repo.eclipse.org/content/groups/releases/"})
